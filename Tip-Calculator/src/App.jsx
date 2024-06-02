a95407102db45c7f2c79b13647f34010ff2ad20e
skm1803 <shubham.mishra0711@gmail.com>
2024-06-02 11:27:18 +0530

    Tip Calculator

---
+++ Tip-Calculator/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import BillInput from "./components/BillInput";
+import SelectPercentage from "./components/SelectPercentage";
+import Output from "./components/Output";
+
+export default function App() {
+  return (
+    <div>
+      <TipCalculator />
+    </div>
+  );
+}
+
+function TipCalculator() {
+  const [bill, setBill] = useState("");
+  const [percentage1, setPercentage1] = useState(0);
+  const [percentage2, setPercentage2] = useState(0);
+
+  const tip = bill * ((percentage1 + percentage2) / 2 / 100);
+
+  function handleReset() {
+    setBill("");
+    setPercentage1(0);
+    setPercentage2(0);
+  }
+
+  return (
+    <div>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage percentage={percentage1} onSelect={setPercentage1}>
+        How did you like the service?
+      </SelectPercentage>
+      <SelectPercentage percentage={percentage2} onSelect={setPercentage2}>
+        How did your friend like the service?
+      </SelectPercentage>
+
+      {bill > 0 && (
+        <>
+          <Output bill={bill} tip={tip} />
+          <Reset onReset={handleReset} />
+        </>
+      )}
+    </div>
+  );
+}
+
+
+function Reset({ onReset }) {
+  return <button onClick={onReset}>Reset</button>;
+}
---
+++ Tip-Calculator/src/components/Reset.jsx
@@ -0,0 +1,4 @@
+function Reset({ onReset }) {
+    return <button onClick={onReset}>Reset</button>;
+  }
+export default Reset;  
